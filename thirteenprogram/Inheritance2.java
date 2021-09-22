e379e4ff19aae1c0bcbd9ad17d643fc4e88ac18d
areenasjayan333 <62836333+areenasjayan333@users.noreply.github.com>
2021-09-23 00:35:41 +0530

    done

---
+++ thirteenprogram/Inheritance2.java
@@ -0,0 +1,12 @@
+/*Suppose your institution wishes to maintain a database of its employees. The database
+is divided into a number of classes whose (Assistant Professor, Associate Professor,
+Professor etc). Construct a Super Class Person to store personal information. Derive
+all the above classes from the Class. Develop a Java program to specify all the classes
+and define functions to create the database and retrieve individual information as and
+when required. Support at least 5 employees on each category.*/
+
+package thirteenprogram;
+
+public class Inheritance2 {
+
+}
